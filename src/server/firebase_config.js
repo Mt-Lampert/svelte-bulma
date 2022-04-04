d6234b6e0a479cb47ac0cdfae76fc720c2dc0005
Rowan Kh <rowan.kh1@gmail.com>
2022-04-04 12:02:56 +0200

    w: firebase.config

---
+++ src/server/firebase_config.js
@@ -0,0 +1,10 @@
+// src/firebase_config.ts
+export default {
+  // meine firebase config object here
+  apiKey: "AIzaSyBdGQukTxX-x5Ai2SPl2V7Gdv_u9648u88",
+  authDomain: "serverless-chat-ee480.firebaseapp.com",
+  projectId: "serverless-chat-ee480",
+  storageBucket: "serverless-chat-ee480.appspot.com",
+  messagingSenderId: "471097305051",
+  appId: "1:471097305051:web:04310e108a8f722e7429fe",
+};
