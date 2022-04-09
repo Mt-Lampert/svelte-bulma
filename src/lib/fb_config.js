fe907aad4387da5b2e9a6b2413d8eca365b77036
Mt-Lampert <perrin.blog@web.de>
2022-04-09 15:56:37 +0200

    w: setting up the firebase credentials

---
+++ src/lib/fb_config.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: "AIzaSyCHcrcaTkPLRZFbdcssab3W_SQGtW0sKcs",
+  authDomain: "boom-kid-store.firebaseapp.com",
+  projectId: "boom-kid-store",
+  storageBucket: "boom-kid-store.appspot.com",
+  messagingSenderId: "950750988712",
+  appId: "1:950750988712:web:3cd3832609f2e5f449359d",
+  measurementId: "G-NK641E42MP",
+};
